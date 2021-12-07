2a0a2a5f9fbba18cb8cddad0f09e08cf255b64a0
xhayper <hayper1919@gmail.com>
2021-12-07 17:31:19 +0700

    Better Day 5

--- 2021/Day 5/lua/code.lua
+++ 2021/Day 5/lua/code.lua
@@ -1,74 +1,18 @@
-function dofile(filename) return assert(loadfile(filename))() end
+function dofile(filename)
+	return assert(loadfile(filename))()
+end
 
 local Array = dofile("..\\..\\Common\\lua\\Array.lua")
+local Utility = dofile("..\\..\\Common\\lua\\Utility.lua")
 
 local code = {}
 
--- https://stackoverflow.com/questions/1426954/split-string-in-lua
---
--- gsplit: iterate over substrings in a string separated by a pattern
---
--- Parameters:
--- text (string)    - the string to iterate over
--- pattern (string) - the separator pattern
--- plain (boolean)  - if true (or truthy), pattern is interpreted as a plain
---                    string, not a Lua pattern
---
--- Returns: iterator
---
--- Usage:
--- for substr in gsplit(text, pattern, plain) do
---   doSomething(substr)
--- end
-local function gsplit(text, pattern, plain)
-	local splitStart, length = 1, #text
-	return function()
-		if splitStart then
-			local sepStart, sepEnd = string.find(text, pattern, splitStart, plain)
-			local ret
-			if not sepStart then
-				ret = string.sub(text, splitStart)
-				splitStart = nil
-			elseif sepEnd < sepStart then
-				-- Empty separator!
-				ret = string.sub(text, splitStart, sepStart)
-				if sepStart < length then
-					splitStart = sepStart + 1
-				else
-					splitStart = nil
-				end
-			else
-				ret = sepStart > splitStart and string.sub(text, splitStart, sepStart - 1) or ""
-				splitStart = sepEnd + 1
-			end
-			return ret
-		end
-	end
-end
-
--- split: split a string into substrings separated by a pattern.
---
--- Parameters:
--- text (string)    - the string to iterate over
--- pattern (string) - the separator pattern
--- plain (boolean)  - if true (or truthy), pattern is interpreted as a plain
---                    string, not a Lua pattern
---
--- Returns: table (a sequence table containing the substrings)
-local function split(text, pattern, plain)
-	local ret = Array()
-	for match in gsplit(text, pattern, plain) do
-		ret:push(match)
-	end
-	return ret
-end
-
 function code:part1(input)
-	local point = split(input, "\n", true)
+	local point = Utility:split(input, "\n", true)
 	local map = Array()
 	for _, raw in pairs(point) do
-		local res = split(raw, " -> ", true):map(function(x)
-			return split(x, ",", true):map(function(x)
+		local res = Utility:split(raw, " -> ", true):map(function(x)
+			return Utility:split(x, ",", true):map(function(x)
 				return tonumber(x)
 			end)
 		end)
@@ -135,11 +79,11 @@ function code:part1(input)
 end
 
 function code:part2(input)
-	local point = split(input, "\n", true)
+	local point = Utility:split(input, "\n", true)
 	local map = Array()
 	for _, raw in pairs(point) do
-		local res = split(raw, " -> ", true):map(function(x)
-			return split(x, ",", true):map(function(x)
+		local res = Utility:split(raw, " -> ", true):map(function(x)
+			return Utility:split(x, ",", true):map(function(x)
 				return tonumber(x)
 			end)
 		end)
