18f8f7ea3efa482a77c13f5dabf2b66ae7a2e04e
xhayper <hayper1919@gmail.com>
2021-12-07 17:41:29 +0700

    Day 6 - Lua

--- 2021/Day 6/lua/code.lua
+++ 2021/Day 6/lua/code.lua
@@ -1,11 +1,77 @@
+function dofile(filename)
+	return assert(loadfile(filename))()
+end
+
+local Utility = dofile("..\\..\\Common\\lua\\Utility.lua")
+
 local code = {}
 
 function code:part1(input)
-	return 0
+	local ageObject = {}
+
+	Utility
+		:split(input, ",", true)
+		:map(function(x)
+			return tonumber(x)
+		end)
+		:forEach(function(v)
+			ageObject[v] = (ageObject[v] and ageObject[v] + 1) or 1
+		end)
+
+	for day = 1, 80 do
+		local newAgeObject = {}
+		for k, v in pairs(ageObject) do
+			if k == 0 then
+				newAgeObject[6] = (newAgeObject[6] or 0) + v
+				newAgeObject[8] = (newAgeObject[8] or 0) + v
+			else
+				newAgeObject[k - 1] = (newAgeObject[k - 1] or 0) + v
+			end
+		end
+		ageObject = newAgeObject
+	end
+
+	local fishCount = 0
+
+	for _, v in pairs(ageObject) do
+		fishCount = fishCount + v
+	end
+
+	return fishCount
 end
 
 function code:part2(input)
-	return 0
+	local ageObject = {}
+
+	Utility
+		:split(input, ",", true)
+		:map(function(x)
+			return tonumber(x)
+		end)
+		:forEach(function(v)
+			ageObject[v] = (ageObject[v] and ageObject[v] + 1) or 1
+		end)
+
+	for day = 1, 256 do
+		local newAgeObject = {}
+		for k, v in pairs(ageObject) do
+			if k == 0 then
+				newAgeObject[6] = (newAgeObject[6] or 0) + v
+				newAgeObject[8] = (newAgeObject[8] or 0) + v
+			else
+				newAgeObject[k - 1] = (newAgeObject[k - 1] or 0) + v
+			end
+		end
+		ageObject = newAgeObject
+	end
+
+	local fishCount = 0
+
+	for _, v in pairs(ageObject) do
+		fishCount = fishCount + v
+	end
+
+	return fishCount
 end
 
 return code
