0e0704c19fe2c5e4a72d840cd07c9f79ed0611e8
xhayper <hayper1919@gmail.com>
2022-12-08 09:36:16 +0700

    feat: 2022, day 4

---
+++ 2022/Day 4/src/main.rs
@@ -0,0 +1,13 @@
+use crate::input::{example_input, first_example_answer, input, second_example_answer};
+use crate::solution::{part_1, part_2};
+
+mod input;
+mod solution;
+
+fn main() {
+    assert_eq!(part_1(example_input()), first_example_answer());
+    assert_eq!(part_2(example_input()), second_example_answer());
+
+    println!("Day 4 Part 1's answer: {}", part_1(input()));
+    println!("Day 4 Part 2's answer: {}", part_2(input()));
+}
---
+++ 2022/Day 4/src/solution.rs
@@ -0,0 +1,46 @@
+use std::ops::Range;
+
+pub fn get_range(input: &str) -> Range<u32> {
+    let range = input.split("-").collect::<Vec<&str>>();
+
+    Range {
+        start: range[0].parse::<u32>().unwrap(),
+        end: range[1].parse::<u32>().unwrap(),
+    }
+}
+
+pub fn part_1(input: String) -> String {
+    let mut overlap: u32 = 0;
+
+    for line in input.lines() {
+        let pair = line.split(",").collect::<Vec<&str>>();
+
+        let first_range = get_range(pair[0]);
+        let second_range = get_range(pair[1]);
+
+        if first_range.start <= second_range.start && first_range.end >= second_range.end {
+            overlap += 1;
+        } else if second_range.start <= first_range.start && second_range.end >= first_range.end {
+            overlap += 1;
+        }
+    }
+
+    overlap.to_string()
+}
+
+pub fn part_2(input: String) -> String {
+    let mut overlap: u32 = 0;
+
+    for line in input.lines() {
+        let pair = line.split(",").collect::<Vec<&str>>();
+
+        let first_range = get_range(pair[0]);
+        let second_range = get_range(pair[1]);
+
+        if first_range.start <= second_range.end && first_range.end >= second_range.start {
+            overlap += 1;
+        }
+    }
+
+    overlap.to_string()
+}
